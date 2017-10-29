cf0d383301707af7a43127ef1577638eaedeb084
katsuhisa91 <91katsuhisa@gmail.com>
2017-10-29 15:56:31 +0900

    generated by kaminari

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,6 +1,7 @@
 class TopController < ApplicationController
   def index
-    # @restaurants = Restaurant.all.includes(:pref, :category)
-    @restaurants = Restaurant.search(params)
+    # page(params[:page]) - ページ番号を取得(paginateヘルパーがいいかんじにやってくれる)
+    # per(40) - 1ページあたりの表示数 40件
+    @restaurants = Restaurant.search(params).page(params[:page]).per(40)
   end
 end
